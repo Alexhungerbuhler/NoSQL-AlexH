79ee169ce326360f6704d4b78155937622e04541
Alex Hungi <alexandre.hungerbuhler@heig-vd.ch>
2024-10-17 12:03:59 +0200

    Modification de la connexion serveur

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import pouch from 'pouchdb'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/test">Test</RouterLink>
+        <RouterLink to="/pageTest">Test</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.ts
+++ src/router/index.ts
@@ -18,8 +18,8 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/pagetest',
-      name: 'pagetest',
+      path: '/pageTest',
+      name: 'pageTest',
       component: () => import('../views/pageTest.vue')
     }
   ]
